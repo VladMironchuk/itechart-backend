eddbeecba99fd74c49e3001520610d79a5418fd3
Vladislav Mironchuk <vlad.mironchuk.02@gmail.com>
2021-12-06 18:41:24 +0300

    add: add order list validation

--- src/routes/authenticate.ts
+++ src/routes/authenticate.ts
@@ -3,12 +3,13 @@ import * as jwt from 'jsonwebtoken';
 const passport = require("passport");
 import { serverConfig } from '../config/server-config';
 import randtoken from 'rand-token'
+import validateOrderList from '../utils/order-list'
 
 const router = Router();
 export const refreshTokens = {}
 
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
-  passport.authenticate('local', {session: false}, (err, user, { message }) => {
+  passport.authenticate('local', {session: false}, async (err, user, { message }) => {
     if (err || !user) {
       return res.status(400).json({
         message
@@ -21,7 +22,7 @@ router.post('/', async (req: Request, res: Response, next: NextFunction) => {
 
     const refreshToken = randtoken.uid(256)
     refreshTokens[refreshToken] = user.username
-
+    await validateOrderList(user.id)
     return res.send({ userId: user.id, token, refreshToken });
   })(req, res);
 });
